0db54a7da469c3f000a95e487cbf76ae7cb52665
israeldail <israeldail2@gmail.com>
2025-07-05 15:11:12 -0400

    disable button at certain thresholds to prevent crashes. made die dynamic in size for clean look.

--- DiceRoller/ContentView.swift
+++ DiceRoller/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+    
     var body: some View {
         VStack {
-            DiceView()
+            Text("Dice Roller")
+                .font(.largeTitle.lowercaseSmallCaps())
+            
+            HStack {
+                ForEach(1...numberOfDice, id: \.description) { _ in
+                        DiceView()
+                }
+            }
+            
+            HStack {
+                Button("Remove Dice") {
+                   withAnimation {
+                       numberOfDice -= 1
+                    }
+                }
+                .disabled(numberOfDice == 1)
+                
+                Button("Add Dice") {
+                    withAnimation {
+                        numberOfDice += 1
+                    }
+                }
+                .disabled(numberOfDice == 5)
+            }
+            .padding()
         }
         .padding()
     }
