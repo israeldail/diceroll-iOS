95ee5f768ff2684c666c0d245ac7dce0feed1ae1
israeldail <israeldail2@gmail.com>
2025-07-05 14:48:17 -0400

    removed boilerplate, added dice ciew to content view. added state property and added logic to roll button.

--- DiceRoller/DiceView.swift
+++ DiceRoller/DiceView.swift
@@ -6,13 +6,25 @@
 //
 
 import SwiftUICore
+import SwiftUI
 
 struct DiceView: View {
     // number of pips on die
     // another comment
-    var numberOfPips: Int = 1
+    @State private var numberOfPips: Int = 1
     
     var body: some View {
-        Image(systemName: "die.face.\(numberOfPips)")
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips)")
+                .resizable()
+                .frame(width: 100, height: 100)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
     }
 }
